52473af574bdaee443969f8ae5adc1740304f3ec
Brandon Roehl <me@brandonroehl.org>
2025-03-16 09:09:30 -0500

    move into the project

---
+++ elaborate/CodeView/CodeAttachment.swift
@@ -0,0 +1,10 @@
+//
+//  CodeAttachment.swift
+//  CodeView
+//
+//  Created by Brandon Roehl on 3/16/25.
+//
+
+class CodeAttachment: NSTextAttachment {
+    weak var codeViewDelegate: CodeViewDelegate?
+}
