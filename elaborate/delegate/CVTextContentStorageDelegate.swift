249de4bb90a5e38c5cf8ab4fcda9b58100be61b8
Brandon Roehl <me@brandonroehl.org>
2025-04-20 12:42:48 -0500

    more delegate methods to see if any of these do what I want

--- elaborate/delegate/CVTextContentStorageDelegate.swift
+++ elaborate/delegate/CVTextContentStorageDelegate.swift
@@ -53,44 +53,45 @@ extension CVCoordinator: NSTextContentStorageDelegate {
     // MARK: - NSTextContentManagerDelegate
     
     public func textContentManager(_ textContentManager: NSTextContentManager,
-                               textElementAt location: NSTextLocation) -> NSTextElement? {
-            // This method is called when TextKit 2 needs a text element at a specific location
-            // You can customize and return a different NSTextElement than the default
-            
-            // Get the offset from the start of the document to this location
-            let offset = textContentManager.offset(from: textContentManager.documentRange.location, to: location)
-            
-            // For demonstration, we'll log the offset
-            print("TextKit2 is requesting text element at offset: \(offset)")
-            
-            // You can return nil to use the default text element, or create a custom one
-            // For example, you might want to customize how certain paragraphs are displayed:
-            
-            /*
-            // Example of returning a custom text element:
-            let range = NSRange(location: offset, length: 10) // Define appropriate range
-            if let textStorage = (textContentManager as? NSTextContentStorage)?.textStorage,
-               range.location + range.length <= textStorage.length {
-                let customAttributedString = NSMutableAttributedString(attributedString:
-                    textStorage.attributedSubstring(from: range))
-                
-                // Apply custom attributes
-                customAttributedString.addAttribute(.foregroundColor, value: NSColor.red,
-                    range: NSRange(location: 0, length: customAttributedString.length))
-                
-                // Create and return a custom paragraph
-                return NSTextParagraph(attributedString: customAttributedString)
-            }
-            */
-            
-            // Return nil to use the default text element
-            return nil
-        }
+                                   textElementAt location: NSTextLocation) -> NSTextElement? {
+        // This method is called when TextKit 2 needs a text element at a specific location
+        // You can customize and return a different NSTextElement than the default
         
+        // Get the offset from the start of the document to this location
+        let offset = textContentManager.offset(from: textContentManager.documentRange.location, to: location)
+        
+        // For demonstration, we'll log the offset
+        print("TextKit2 is requesting text element at offset: \(offset)")
+        
+        // You can return nil to use the default text element, or create a custom one
+        // For example, you might want to customize how certain paragraphs are displayed:
+        
+//        let textStorage = (textContentManager as! NSTextContentStorage)
+        /*
+         // Example of returning a custom text element:
+         let range = NSRange(location: offset, length: 10) // Define appropriate range
+         if let textStorage = (textContentManager as? NSTextContentStorage)?.textStorage,
+         range.location + range.length <= textStorage.length {
+         let customAttributedString = NSMutableAttributedString(attributedString:
+         textStorage.attributedSubstring(from: range))
+         
+         // Apply custom attributes
+         customAttributedString.addAttribute(.foregroundColor, value: NSColor.red,
+         range: NSRange(location: 0, length: customAttributedString.length))
+         
+         // Create and return a custom paragraph
+         return NSTextParagraph(attributedString: customAttributedString)
+         }
+         */
+        
+        // Return nil to use the default text element
+        return nil
+    }
+    
     public func textContentManager(_ textContentManager: NSTextContentManager,
-                               shouldEnumerate textElement: NSTextElement,
-                               options: NSTextContentManager.EnumerationOptions = []) -> Bool {
-            // Control whether this text element should be included in layout
-            return true
-        }
+                                   shouldEnumerate textElement: NSTextElement,
+                                   options: NSTextContentManager.EnumerationOptions = []) -> Bool {
+        // Control whether this text element should be included in layout
+        return true
+    }
 }
