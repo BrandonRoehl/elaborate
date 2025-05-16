43a4a8971546a4d32e4766a73ae019280debab33
Brandon Roehl <me@brandonroehl.org>
2025-05-16 16:52:39 -0500

    work on packaging

--- elb/Package.swift
+++ elb/Package.swift
@@ -4,21 +4,31 @@
 import PackageDescription
 
 let package = Package(
-    name: "elb",
+    name: "Elb",
+    platforms: [
+        .macOS("13"),
+        .iOS("16"),
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "elb",
-            targets: ["elb"]),
+            name: "Elb",
+            targets: ["Elb", "ElbLib"])
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "elb"),
+            name: "Elb",
+            dependencies: ["ElbLib"]
+        ),
+        .binaryTarget(
+            name: "ElbLib",
+            path: "./lib/Elb.xcframework"
+        ),
         .testTarget(
-            name: "elbTests",
-            dependencies: ["elb"]
+            name: "ElbTests",
+            dependencies: ["Elb", "ElbLib"],
         ),
     ]
 )
