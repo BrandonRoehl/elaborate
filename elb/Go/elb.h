db3d320c99db47731b8623c52887ecf4ba5693af
Brandon Roehl <me@brandonroehl.org>
2025-05-16 18:46:28 -0500

    h

---
+++ elb/Go/elb.h
@@ -0,0 +1,25 @@
+#ifndef ELB_H
+#define ELB_H
+#include <stdlib.h>
+
+typedef enum Status {
+    ERROR = 0,
+    VALUE = 1,
+    EOF = 2,
+    INFO = 3
+} Status;
+
+typedef struct Result {
+    char* output;
+    int64_t line;
+    Status status;
+} Result;
+
+typedef struct Response {
+    Result **result;
+    int64_t size;
+} Response;
+
+Response Execute(const char *content);
+
+#endif
