9a0d2052ead2e7305b55bd392b18d9957afe3c5a
Brandon Roehl <me@brandonroehl.org>
2025-05-18 13:27:14 -0500

    elb to swift again

--- Elb/Sources/Elb/elb.swift
+++ Elb/Sources/Elb/elb.swift
@@ -3,11 +3,26 @@
 import ElbLib
 
 public struct Response {
-    public enum Status: Int {
-        case ERROR = 0
-        case VALUE = 1
-        case EOF = 2
-        case INFO = 3
+    public enum Status {
+        case ERROR
+        case VALUE
+        case EOF
+        case INFO
+        
+        fileprivate init?(_ rawValue: ElbLib.Status) {
+            switch rawValue {
+            case ElbLib.ERROR:
+                self = .ERROR
+            case ElbLib.VALUE:
+                self = .VALUE
+            case ElbLib.EOF:
+                self = .EOF
+            case ElbLib.INFO:
+                self = .INFO
+            default:
+                return nil
+            }
+        }
     }
 
     let status: Status
@@ -18,5 +33,15 @@ public struct Response {
 public func elbExecute(_ document: String) -> [Response] {
     return document.withCString { cString in
         let response = ElbLib.Execute(cString)
+        let size = Int(response.size)
+        defer {
+            // Free the C transport memory after constructing a swift struct
+            response.results.deinitialize(count: size)
+        }
+        return (0..<size).map { i in
+            let result = response.results.advanced(by: i).pointee
+            let output = String(cString: result.output)
+            return Response(status: .init(result.status)!, line: Int(clamping: result.line), ouput: output)
+        }
     }
 }
