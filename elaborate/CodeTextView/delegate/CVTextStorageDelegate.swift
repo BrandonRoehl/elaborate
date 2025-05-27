e8c5e6aaf59263871cad676b4978cef70f61e104
Brandon Roehl <me@brandonroehl.org>
2025-05-26 22:01:33 -0500

    unify

--- elaborate/CodeTextView/delegate/CVTextStorageDelegate.swift
+++ elaborate/CodeTextView/delegate/CVTextStorageDelegate.swift
@@ -14,19 +14,20 @@
 #endif
 
 extension CVCoordinator: NSTextStorageDelegate {
-    
     public func textStorage(
         _ textStorage: NSTextStorage,
-        willProcessEditing editedMask: OSTextStorageEditActions,
+        didProcessEditing editedMask: OSTextStorageEditActions,
         range editedRange: NSRange,
         changeInLength delta: Int
     ) {
         guard editedMask.contains(.editedCharacters) else { return }
+        let text = textStorage.string
+        
         // The textStorage holds the string it will be and not the string it was
         // so you have to determine where we were before here
         
-#if DEBUG // these are here in this to help debug the code during development
-        let text = textStorage.string
+#if DEBUG
+        // these are here in this to help debug the code during development
         let check: [Int] = text.enumerated().filter(\.element.isNewline).map { (index, _) in
             return index
         }
@@ -36,17 +37,17 @@ extension CVCoordinator: NSTextStorageDelegate {
         let startIndex: Int = self.newlineOffsets.firstIndex(where: { newLine in
             return newLine >= editedRange.lowerBound
         }) ?? self.newlineOffsets.endIndex
-
+        
         let endOffset = (editedRange.upperBound - delta)
         let finalIndex = self.newlineOffsets.firstIndex(where: { newLine in
             return newLine >= endOffset
         }) ?? self.newlineOffsets.count
-
+        
         // update the offset for those that are at final index
         for i in finalIndex..<self.newlineOffsets.count {
             self.newlineOffsets[i] += delta
         }
-
+        
         // Remove the ones we know are bad
         self.newlineOffsets.removeSubrange(startIndex..<finalIndex)
         // The string to look at for changes
@@ -59,26 +60,15 @@ extension CVCoordinator: NSTextStorageDelegate {
         self.newlineOffsets.insert(contentsOf: newOffsets, at: startIndex)
         
         // Good checks in dev but don't use this code in prod far to slow
-        #if DEBUG
+#if DEBUG
         for offset in newlineOffsets {
             let idx = text.index(text.startIndex, offsetBy: offset)
             assert(text[idx].isNewline, "our adjustments don't lead to a \n")
         }
         assert(check == newlineOffsets, "Somehow we lost count and newlines aren't aligned")
-        #endif
-    }
-    
-    public func textStorage(
-        _ textStorage: NSTextStorage,
-        didProcessEditing editedMask: OSTextStorageEditActions,
-        range editedRange: NSRange,
-        changeInLength delta: Int
-    ) {
-        guard editedMask.contains(.editedCharacters) else { return }
+#endif
         
         // Add in the new paragraph markers
-        
-        let text = textStorage.string
         Task.detached { @MainActor in
             self.syncHeights()
             if self.text.wrappedValue != text {
